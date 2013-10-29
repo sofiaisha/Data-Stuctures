aa0fbcf3a5eb2d10cbfe7e3b8ac3d56489984465
Julien SIMON <julien@julien.org>
2013-10-29 23:07:23 +0100

    Added debug_print_simple()

--- util.h
+++ util.h
@@ -3,15 +3,22 @@
 
 #include <stdio.h>
 
-#ifdef DEBUG
+#ifdef DEBUG1
 #define debug_print(FMT, ...)  \
-	if (DEBUG) \
+	if (DEBUG1) \
 		 do { fprintf(stderr, "%s() in %s, line %i: " FMT "\n", \
 			__func__, __FILE__, __LINE__, ##__VA_ARGS__);   \
 		 } while (0);
-		
 #else
 #define debug_print(FMT, ...) 
+#endif
+
+#ifdef DEBUG2
+#define debug_print_simple(FMT, ...)  \
+	if (DEBUG2) \
+		 do { fprintf(stderr, FMT " ", ##__VA_ARGS__); } while (0);
+#else
+#define debug_print_simple(FMT, ...) 
 #endif
 
 #endif

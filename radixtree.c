b511e78e2e2be329c64c9f70990fcf688a4f6ba7
Julien SIMON <julien@julien.org>
2015-03-16 16:25:04 +0100

    Add comments. Avoid allocating childs array for leaf nodes (memory saving) --> https://github.com/juliensimon/data-structures/issues/4

--- radixtree.c
+++ radixtree.c
@@ -54,25 +54,42 @@ char* radixtree_add(radixtree_t* t, char* s) {
 	rtnode_t** currentChilds = t->childs;
 
 	for (unsigned int i=0;i<strlen(s);i++) {
+		// Compute the index of the character in the childs array
 		unsigned int childIndex = s[i] - t->alphabetStart;
-
+		// If we're on a former leaf, add a node 
 		if (currentChilds[childIndex] == NULL) {
 			currentChilds[childIndex] = (rtnode_t*)malloc(sizeof(rtnode_t));
 			if (currentChilds[childIndex] == NULL) {
                 		perror("radixtree_add: can't create childs, errno=%d");
 				return NULL;
 			}
-			// If this is the last character, mark the node
-			currentChilds[childIndex]->last = (i == strlen(s)-1) ? true : false;
+			currentChilds[childIndex]->last   = false;
+			currentChilds[childIndex]->childs = NULL;
+		}
 
-			currentChilds[childIndex]->childs = (rtnode_t**)malloc(t->alphabetSize*sizeof(rtnode_t*));
+		// If this is the last character, 
+		// Mark the node
+		// Don't allocate childs array to save memory
+		// Leave the loop
+		if (i == strlen(s)-1) {
+			currentChilds[childIndex]->last = true;
+			break;
+		}
+		
+		// This is either a new node (allocated above) or a former leaf node
+		// Allocate childs array
+		if (currentChilds[childIndex]->childs == NULL) {
+			currentChilds[childIndex]->childs = 
+				(rtnode_t**)malloc(t->alphabetSize*sizeof(rtnode_t*));
 			if (currentChilds[childIndex]->childs == NULL) {
-                		perror("radixtree_add: can't create childs, errno=%d");
+               			perror("radixtree_add: can't create childs, errno=%d");
 				free(currentChilds[childIndex]);
 				return NULL;
 			}
+			// Zero the array (yes, it's really needed)
 			memset(currentChilds[childIndex]->childs, 0, t->alphabetSize * sizeof(rtnode_t*));
 		}
+		// Walk to the next node
 		currentChilds = currentChilds[childIndex]->childs;
 	}
 
@@ -88,13 +105,20 @@ char* radixtree_find(radixtree_t* t, char* s) {
 	assert (t->childs != NULL);
 
 	rtnode_t** currentChilds = t->childs;
-	rtnode_t* child;	
+	rtnode_t* child = NULL;	
 	for (unsigned int i=0;i<strlen(s);i++) {
+		if (currentChilds == NULL) {
+			// We're on a leaf and we have more characters to look for : string is not found
+			return NULL;
+		}
+		// Compute the index of the character in the childs array
 		unsigned int childIndex = s[i] - t->alphabetStart;
 		child = currentChilds[childIndex];
 		if (child == NULL) {
+			// We're inside the tree, but no node exists for this character : string is not found
 			return NULL;
 		}
+		// Move to the next node
 		currentChilds = child->childs;
 	}
 
@@ -119,7 +143,7 @@ float radixtree_density(radixtree_t* t, rtnode_t** childs, unsigned int* total, 
 		}
 	}
 	unsigned int full = (*total)-(*empty);
-	debug_print("Total: %ud, empty: %iud, full: %ud\n", *total, *empty, full);
+	debug_print("Total: %u, empty: %u, full: %u\n", *total, *empty, full);
 	return ((float)full)/(*total)*100;
 }
 
@@ -142,9 +166,11 @@ void radixtree_destroy_internal(radixtree_t* t, rtnode_t* c) {
 	if ( (t == NULL) || (c == NULL) ) {
 		return;
 	}
-	for (unsigned int i=0;i<t->alphabetSize;i++) {
-		radixtree_destroy_internal(t, c->childs[i]);
+	if (c->childs) {
+		for (unsigned int i=0;i<t->alphabetSize;i++) {
+			radixtree_destroy_internal(t, c->childs[i]);
+		}
+		free(c->childs);
 	}
-	free(c->childs);
 	free(c);
 }

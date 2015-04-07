b228830a70dc24ae2b9bc230daeb1ae456655a2f
Julien SIMON <julien@julien.org>
2015-04-07 09:05:47 +0200

    Move fields to the end of the structure to allow casts to binary tree structures

--- rbtree.h
+++ rbtree.h
@@ -6,20 +6,20 @@ enum colors {RED, BLACK};
 typedef struct rbnode_t rbnode_t;
 struct rbnode_t {
         void*           elem;
-	enum colors	color;
         rbnode_t*       parent;
         rbnode_t*       left;
         rbnode_t*       right;
+	enum colors	color;
 };
 
 typedef struct {
         rbnode_t*       root;
-	rbnode_t*	nil;
         unsigned int    size;
         int             (*compare)(void*, void*);
         void            (*print)(void*, FILE*);
         void*           (*clone)(void*);
         void            (*destroy)(void*);
+	rbnode_t*	nil;
 } rbtree_t;
 
 #endif
